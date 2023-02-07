8d4a4c1df67a9a103bd060308b68422365c06269
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-02-07 21:40:42 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,32 +4,39 @@
 
 int main() {
     std::ifstream file("C++.txt");
-    std::string fileInLine;
+    std::string fileInLine, temp;
+
     int counter = 0;
     if (file.is_open()) {
-        char letter; std::cout << "Write letter: ";
+        while (std::getline(file, temp)) {}
+        std::cout << temp;
+        char letter; std::cout << "\n\nWrite letter: ";
         std::cin >> letter;
-        while (std::getline(file, fileInLine)) {
-            for (int i = 0; i < fileInLine.size(); i++) {
-                int word = 0;
-                if (fileInLine[i] == ' ') {
-                    word++;
-                    i++;
-                }
-                if (letter == fileInLine[i] && word == 1) {
+
+        fileInLine += ' ';
+        fileInLine += temp;
+        for (int i = 0; i < fileInLine.size(); i++) {
+            if (fileInLine[i] == ' ' ||
+                fileInLine[i] == '"' ||
+                fileInLine[i] == '(' ||
+                fileInLine[0] == ' ')
+            {
+
+                if (letter == fileInLine[i + 1] ||
+                    fileInLine[0] == fileInLine[1])
                     counter++;
-                }
+
             }
         }
         file.close();
     }
-    std::cout << counter;
+    std::cout << "Your letter occurs: " << counter << " times";
     return 0;
 }
 
 /*
-C++ (pronounced "C plus plus") is a high-level general-purpose programming language created by Danish computer scientist 
-Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". The language has expanded significantly 
+C++ (pronounced "C plus plus") is a high-level general-purpose programming language created by Danish computer scientist
+Bjarne Stroustrup as an extension of the C programming language, or "C with Classes". The language has expanded significantly
 over time, and modern C++ now has object-oriented, generic, and functional features in addition to facilities for low-level memory
 manipulation. It is almost always implemented as a compiled language, and many vendors provide C++ compilers,
 including the Free Software Foundation, LLVM, Microsoft, Intel, Embarcadero, Oracle, and IBM, so it is available on many platforms.
